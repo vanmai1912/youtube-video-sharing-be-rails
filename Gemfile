d3befa4bddfac59af70657211e71ddc0bc2ca1a4
vanmai1912 <mai.dv01@gmail.com>
2024-09-13 22:10:56 +0700

    custom confirmation register

--- Gemfile
+++ Gemfile
@@ -13,6 +13,10 @@ gem "sqlite3", ">= 1.4"
 
 gem "pg"
 
+gem 'devise'
+
+gem 'dotenv-rails'
+
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", ">= 5.0"
 
@@ -54,6 +58,8 @@ end
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem "web-console"
+
+  gem "letter_opener"
 
   # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
   # gem "rack-mini-profiler"
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,20 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  config.action_controller.asset_host = ENV['FRONTEND_URL']
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # SMTP settings for Gmail
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    user_name: ENV['ADDRESS'],
+    password: ENV['PASSWORD'],
+    authentication: "plain",
+    enable_starttls_auto: true
+  }
+
 end

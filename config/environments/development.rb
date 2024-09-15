485f6769c801443ef77f4154e7d0cdb1625846af
vanmai1912 <mai.dv01@gmail.com>
2024-09-15 13:16:27 +0700

    add url

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -79,6 +79,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.hosts << /.+\.trycloudflare.com/
+  config.hosts << "youtube-video-sharing-ngix.onrender.com"
 
   # SMTP settings for Gmail
   config.action_mailer.smtp_settings = {
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.hosts << "youtube-video-sharing-ngix.onrender.com"
+
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
